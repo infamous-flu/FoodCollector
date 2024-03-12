0f1656b6605d4970d05b9c2132acc56e94e2cb40
cyborgbeta <s3590712@vuw.leidenuniv.nl>
2024-03-12 17:16:14 +0100

    maybe maybe maybe maybe maybe

--- Python/reset.py
+++ Python/reset.py
@@ -1,6 +1,7 @@
 import argparse
 from dataclasses import dataclass
 from typing import List
+import numpy as np
 from peaceful_pie.unity_comms import UnityComms
 
 
@@ -25,10 +26,29 @@ class RLResult:
     observation: RayResults
 
 
+def _ray_results_to_np_array(ray_results: RayResults):
+    distances_np = np.array(ray_results.rayDistances)
+    distances_np = 1 / distances_np
+    object_types_np = np.array(ray_results.rayHitObjectTypes)
+
+    _obs = np.zeros(
+        (ray_results.NumObjectTypes + 1, *distances_np.shape), dtype=np.float32
+    )
+    np.put_along_axis(
+        _obs,
+        np.expand_dims(object_types_np, axis=0),
+        np.expand_dims(distances_np, axis=0),
+        axis=0,
+    )
+
+    _obs = _obs[:-1]
+    return _obs
+
+
 def run(args: argparse.Namespace) -> None:
     unity_comms = UnityComms(port=args.port)
     res = unity_comms.reset(ResultClass=RayResults)
-    print(res)
+    print(_ray_results_to_np_array(res.observation))
 
 
 if __name__ == "__main__":
